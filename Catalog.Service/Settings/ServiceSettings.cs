68595fc3e1e116295d15650032d605e14bb247ee
Will <69020009+snedex@users.noreply.github.com>
2022-01-26 15:11:57 -0500

    Setting up for IoC and DI

---
+++ Catalog.Service/Settings/ServiceSettings.cs
@@ -0,0 +1,8 @@
+namespace Catalog.Service.Settings
+{
+    //Represents the ServiceSettings in appsettings.json
+    public class ServiceSettings
+    {
+        public string ServiceName { get; init; }
+    }
+}
