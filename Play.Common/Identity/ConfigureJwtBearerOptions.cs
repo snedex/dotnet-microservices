3363af3e357c1adb879a591de2de417a38dd1a13
Will <69020009+snedex@users.noreply.github.com>
2022-02-08 11:01:21 -0500

    Configured standard names for claims and roles

--- Play.Common/Identity/ConfigureJwtBearerOptions.cs
+++ Play.Common/Identity/ConfigureJwtBearerOptions.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Play.Common.Identity
 {
@@ -27,12 +28,19 @@ namespace Play.Common.Identity
                 var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
                 options.Authority = serviceSettings.Authority;
                 options.Audience = serviceSettings.ServiceName;
+
+                //Enforce standard names, aspnet core renames these which can cause issues
+                options.MapInboundClaims = false;
+                options.TokenValidationParameters = new TokenValidationParameters() {
+                    NameClaimType = "name",
+                    RoleClaimType = "role"
+                };
             }
         }
 
         public void Configure(JwtBearerOptions options)
         {
-            throw new NotImplementedException();
+            Configure(Options.DefaultName, options);
         }
     }
 }
